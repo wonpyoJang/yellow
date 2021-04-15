cbe45d3aa288b376f6de8e8a6fd7426d8f08395e
wonpyoJang <jangwonpyo0@gmail.com>
2021-04-16 01:12:22 +0900

    [fixed] published drop down menu

--- lib/src/view/album.dart
+++ lib/src/view/album.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_gallery/photo_gallery.dart' as pg;
 import 'package:yellow/src/image_providers/thumbnail_provider.dart';
 import 'package:yellow/src/view/viewer.dart';
 import 'package:yellow/src/widget/select_button.dart';
@@ -13,7 +14,8 @@ class AlbumView extends StatefulWidget {
   _AlbumViewState createState() => _AlbumViewState();
 }
 
-class _AlbumViewState extends State<AlbumView> {
+class _AlbumViewState extends State<AlbumView>
+    with SingleTickerProviderStateMixin {
   List<Album> _albums;
   List<Medium> _media;
   Album _selectedAlbum;
@@ -22,11 +24,16 @@ class _AlbumViewState extends State<AlbumView> {
   int multiSelectStartIndex = 0;
   int multiSelectCurrentIndex = 0;
   int multiSelectCurrentMaxIndex = 0;
+  AnimationController expandController;
+  Animation<double> animation;
+  bool isExpanded = false;
 
   @override
   void initState() {
     super.initState();
     initAsync(context);
+    prepareAnimations();
+    _runExpandCheck();
   }
 
   void initAsync(BuildContext context) async {
@@ -38,87 +45,148 @@ class _AlbumViewState extends State<AlbumView> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (_isLoading == true)
-      return Center(
-          child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.yellow),
-      ));
-    else
-      return WillPopScope(
-        onWillPop: () async {
-          _media = await _albums[0].getMedia();
-          YellowImagePicker.currentAlbumInfo.value.selectedAlbum =
-              _albums[0];
-          YellowImagePicker.currentAlbumInfo.value.media = _media;
-          YellowImagePicker.currentAlbumInfo.value.albums = _albums;
-          return true;
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            title: _albums.length > 1
-                ? DropdownButton<String>(
-                    value: _selectedAlbum.album.name,
-                    icon: const Icon(Icons.arrow_downward),
-                    iconSize: 24,
-                    elevation: 16,
-                    style: const TextStyle(color: Colors.deepPurple),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.deepPurpleAccent,
-                    ),
-                    onChanged: (String newValue) async {
-                      YellowImagePicker.currentAlbumInfo.value =
-                          CurrentAlbumInfo();
-
-                      _selectedAlbum = _albums
-                          .where((item) => item.album.name == newValue)
-                          .toList()[0];
-                      _media = await _selectedAlbum.getMedia();
-                      YellowImagePicker.currentAlbumInfo.value.selectedAlbum =
-                          _selectedAlbum;
-                      YellowImagePicker.currentAlbumInfo.value.media = _media;
-                      YellowImagePicker.currentAlbumInfo.value.albums = _albums;
-                      setState(() {});
-                    },
-                    items: _albums.map<DropdownMenuItem<String>>((Album value) {
-                      return DropdownMenuItem<String>(
-                        value: value.album.name,
-                        child: Text(value.album.name),
-                      );
-                    }).toList(),
-                  )
-                : Text(_albums[0].album.name),
-          ),
-          body: ValueListenableBuilder(
-              valueListenable: YellowImagePicker.currentAlbumInfo,
-              builder:
-                  (BuildContext context, CurrentAlbumInfo data, Widget child) {
-
-                if(data.media == null ||  data.media.length == 0) {
-                  return Center(child: CircularProgressIndicator());
-                } else {
-                  return GridView.builder(
-                    key: gridKey,
-                    scrollDirection: Axis.vertical,
-                    itemCount: data.media.length,
-                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 150,
-                        childAspectRatio: 1,
-                        crossAxisSpacing: 0,
-                        mainAxisSpacing: 0),
-                    itemBuilder: (context, index) {
-                      return buildImageItem(context, data, index);
-                    },
-                  );
-                }
-              }),
-        ),
-      );
+  ///Setting up the animation
+  void prepareAnimations() {
+    expandController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+    animation = CurvedAnimation(
+      parent: expandController,
+      curve: Curves.fastOutSlowIn,
+    );
   }
 
-  Widget buildImageItem(BuildContext context, CurrentAlbumInfo data, int index) {
+  void _runExpandCheck() {
+    expandController.reverse();
+  }
+
+  @override
+  void didUpdateWidget(AlbumView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _runExpandCheck();
+  }
+
+  @override
+  void dispose() {
+    expandController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        _media = await _albums[0].getMedia();
+        YellowImagePicker.currentAlbumInfo.value.selectedAlbum = _albums[0];
+        YellowImagePicker.currentAlbumInfo.value.media = _media;
+        YellowImagePicker.currentAlbumInfo.value.albums = _albums;
+        return true;
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: _albums.length > 1
+                ? GestureDetector(
+                onTap: () {
+                  if (isExpanded) {
+                    expandController.reverse();
+                    isExpanded = false;
+                  } else {
+                    expandController.forward();
+                    isExpanded = true;
+                  }
+                },
+                child: Text(_albums[0].album.name))
+                : Text(""),
+          ),
+          body: Stack(
+            children: [
+              _isLoading
+                  ? Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.yellow),
+                  ))
+                  : ValueListenableBuilder(
+                  valueListenable: YellowImagePicker.currentAlbumInfo,
+                  builder: (BuildContext context, CurrentAlbumInfo data,
+                      Widget child) {
+                    if (data.media == null || data.media.length == 0) {
+                      return Center(child: CircularProgressIndicator());
+                    } else {
+                      return GridView.builder(
+                        key: gridKey,
+                        scrollDirection: Axis.vertical,
+                        itemCount: data.media.length,
+                        gridDelegate:
+                        SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 150,
+                            childAspectRatio: 1,
+                            crossAxisSpacing: 0,
+                            mainAxisSpacing: 0),
+                        itemBuilder: (context, index) {
+                          return buildImageItem(context, data, index);
+                        },
+                      );
+                    }
+                  }),
+              SizeTransition(
+                  axisAlignment: 1.0,
+                  sizeFactor: animation,
+                  child: Positioned.fill(
+                    child: Container(
+                        color: Colors.blue,
+                        child: ListView.builder(
+                            itemCount: _albums.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () async {
+                                  YellowImagePicker.currentAlbumInfo.value =
+                                      CurrentAlbumInfo();
+
+                                  _selectedAlbum = _albums[index];
+                                  _media = await _selectedAlbum.getMedia();
+                                  YellowImagePicker.currentAlbumInfo.value.selectedAlbum =
+                                      _selectedAlbum;
+                                  YellowImagePicker.currentAlbumInfo.value.media = _media;
+                                  expandController.reverse();
+                                  setState(() {});
+                                },
+                                child: Container(
+                                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                                    child: Row(children: [
+                                      Container(width: 75,
+                                        height: 75,
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            fit: BoxFit.cover,
+                                            image: pg.AlbumThumbnailProvider(
+                                              albumId: _albums[index].album.id,
+                                              mediumType: _albums[index].album.mediumType,
+                                              highQuality: true,
+                                            ),
+                                          ),
+                                        ),),
+                                      SizedBox(width: 10),
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(_albums[index].album.name),
+                                          SizedBox(height: 5),
+                                          Text(_albums[index].album.count.toString()),
+                                        ],
+                                      )
+                                    ],)
+                                ),
+                              );
+                            },
+                        )
+                    ),
+                  )),
+            ],
+          )),
+    );
+  }
+
+  Widget buildImageItem(BuildContext context, CurrentAlbumInfo data,
+      int index) {
     GlobalKey gridItemKey = new GlobalKey();
 
     return Stack(
@@ -133,35 +201,41 @@ class _AlbumViewState extends State<AlbumView> {
           onLongPressStart: (details) {
             double tapPositionX = details.globalPosition.dx;
             double tapPositionY = details.globalPosition.dy;
-            int selectedItemIndex = getSelectedIndex(gridItemKey, tapPositionY, tapPositionX);
+            int selectedItemIndex =
+            getSelectedIndex(gridItemKey, tapPositionY, tapPositionX);
             multiSelectStartIndex = selectedItemIndex;
             if (YellowImagePicker.currentAlbumInfo.value
-                    .media[selectedItemIndex].isSelected ==
+                .media[selectedItemIndex].isSelected ==
                 false) {
-              YellowImagePicker.currentAlbumInfo.value.addSelectedMediaByIndex(selectedItemIndex);
+              YellowImagePicker.currentAlbumInfo.value
+                  .addSelectedMediaByIndex(selectedItemIndex);
             }
             setState(() {});
           },
           onLongPressMoveUpdate: (details) {
             double tapPositionX = details.globalPosition.dx;
             double tapPositionY = details.globalPosition.dy;
-            int selectedItemIndex = getSelectedIndex(gridItemKey, tapPositionY, tapPositionX);
+            int selectedItemIndex =
+            getSelectedIndex(gridItemKey, tapPositionY, tapPositionX);
             multiSelectCurrentIndex = selectedItemIndex;
 
-            for(int i = multiSelectStartIndex; i <= selectedItemIndex; ++i) {
-              YellowImagePicker.currentAlbumInfo.value.addSelectedMediaByIndex(i);
+            for (int i = multiSelectStartIndex; i <= selectedItemIndex; ++i) {
+              YellowImagePicker.currentAlbumInfo.value
+                  .addSelectedMediaByIndex(i);
             }
 
-            for(int i = multiSelectCurrentMaxIndex; i > multiSelectCurrentIndex; --i) {
-              YellowImagePicker.currentAlbumInfo.value.removeSelectedMediaByIndex(i);
+            for (int i = multiSelectCurrentMaxIndex;
+            i > multiSelectCurrentIndex;
+            --i) {
+              YellowImagePicker.currentAlbumInfo.value
+                  .removeSelectedMediaByIndex(i);
             }
 
-            if(selectedItemIndex > multiSelectCurrentMaxIndex) {
+            if (selectedItemIndex > multiSelectCurrentMaxIndex) {
               multiSelectCurrentMaxIndex = selectedItemIndex;
             }
 
-            setState(() {
-            });
+            setState(() {});
           },
           onLongPressEnd: (details) {
             multiSelectCurrentIndex = 0;
@@ -172,9 +246,9 @@ class _AlbumViewState extends State<AlbumView> {
             key: gridItemKey,
             decoration: BoxDecoration(
                 border: Border.all(
-              color: Colors.black,
-              width: 1,
-            )),
+                  color: Colors.black,
+                  width: 1,
+                )),
             child: Image(
                 width: 150,
                 height: 150,
@@ -193,25 +267,22 @@ class _AlbumViewState extends State<AlbumView> {
     );
   }
 
-  int getSelectedIndex(GlobalKey<State<StatefulWidget>> gridItemKey, double tapPositionY, double tapPositionX) {
+  int getSelectedIndex(GlobalKey<State<StatefulWidget>> gridItemKey,
+      double tapPositionY, double tapPositionX) {
     RenderBox _box = gridItemKey.currentContext.findRenderObject();
     RenderBox _boxGrid = gridKey.currentContext.findRenderObject();
     Offset position =
-        _boxGrid.localToGlobal(Offset.zero); //this is global position
+    _boxGrid.localToGlobal(Offset.zero); //this is global position
     double gridLeft = position.dx;
     double gridTop = position.dy;
     double gridWidth = _boxGrid.size.width;
-    
+
     double gridPosition = tapPositionY - gridTop;
-    
+
     //Get item position
     int indexX = (gridPosition / _box.size.width).floor().toInt();
-    int indexY =
-        ((tapPositionX - gridLeft) / _box.size.width)
-            .floor()
-            .toInt();
-    int numberInRow =
-        ((gridWidth) / _box.size.width).floor().toInt();
+    int indexY = ((tapPositionX - gridLeft) / _box.size.width).floor().toInt();
+    int numberInRow = ((gridWidth) / _box.size.width).floor().toInt();
     int selectedItemIndex = indexX * numberInRow + indexY;
     return selectedItemIndex;
   }
